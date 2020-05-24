cb84133c68357af703401d5ed1e3b3dd758481f3
Steven Bunn <bunn.steve@gmail.com>
2020-05-24 08:08:08 -0600

    first commit

---
+++ basics.py
@@ -0,0 +1,8 @@
+def print_mean():
+    student_grades = [9.1, 8.8, 7.5]
+    mean = sum(student_grades) / len(student_grades)
+    print("the mean of the grades was", mean)
+
+
+if __name__ == "__main__":
+    print_mean()
